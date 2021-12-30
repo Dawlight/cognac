fbdf85b439d0a354396d35c295c74111ba2dcc25
Johan Eriksson <johan.eriksson@youcal.com>
2021-12-30 09:56:23 +0100

    Empty list in query arguments now generate empty list.

--- lib/value.ex
+++ lib/value.ex
@@ -7,6 +7,7 @@ defmodule Cognac.Value do
   def build(value, pretty?), do: build_value(value, pretty?)
 
   defp build_value(nil, _), do: "null"
+  defp build_value(value, _pretty?) when value == %{}, do: "{}"
   defp build_value({:variable, name}, _), do: ["$", name]
   defp build_value(value, _) when is_binary(value), do: [?", value, ?"]
 
@@ -21,6 +22,8 @@ defmodule Cognac.Value do
   defp build_value(value, pretty?) when is_tuple(value) do
     build_value(Tuple.to_list(value), pretty?)
   end
+
+  defp build_value([], _pretty?), do: "[]"
 
   defp build_value([{_key, _value} | _] = object, pretty?) do
     [?{, build_object(object, pretty?, []), ?}]
@@ -46,8 +49,6 @@ defmodule Cognac.Value do
   end
 
   # List
-
-  defp build_list([], _, _buffer), do: "[]"
 
   defp build_list([value], pretty?, buffer) do
     Enum.reverse([build_value(value, pretty?) | buffer])
