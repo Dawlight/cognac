57499c7c8cd233609c82371ebf02937e8acf5daa
Johan Eriksson <johan.eriksson@pingpayments.com>
2023-11-20 13:29:17 +0100

    Updated versionks

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Add Cognac as a dependency in your `mix.exs` file:
 
 ```elixir
 def deps do
-  [{:cognac, "~> 0.7.0"}]
+  [{:cognac, "~> 0.8.0"}]
 end
 ```
 
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Cognac.MixProject do
   use Mix.Project
 
-  @version "0.7.0"
+  @version "0.8.0"
   @source_url "https://github.com/Dawlight/cognac"
 
   def project do
