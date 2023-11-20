d0c5e00f6c0b03c80d6bd1507b292f32fa6a5adf
Tobias Pettersson Jensen <tobias.pettersson.jensen@pingpayments.com>
2023-11-20 13:10:11 +0100

    Function clause when no subfields are provided

--- lib/query.ex
+++ lib/query.ex
@@ -14,48 +14,69 @@ defmodule Cognac.Query do
         pretty? -> pretty?
       end
 
-    [@opening[pretty?], build_fields(query, 1, pretty?), ?}]
+    subfields? =
+      case options[:subfields] do
+        nil -> true
+        subfields? -> subfields?
+      end
+
+    [@opening[pretty?], build_fields(query, 1, pretty?, subfields?), ?}]
   end
 
-  defp build_fields(query, level, pretty?) when is_map(query) do
-    build_fields(Map.to_list(query), level, pretty?)
+  defp build_fields(query, level, pretty?, subfields?) when is_map(query) do
+    build_fields(Map.to_list(query), level, pretty?, subfields?)
   end
 
-  defp build_fields(query, level, pretty?), do: build_fields(query, level, pretty?, [])
+  defp build_fields(query, level, pretty?, subfields?),
+    do: build_fields(query, level, pretty?, subfields?, [])
 
-  defp build_fields([], _level, _pretty?, buffer), do: Enum.reverse(buffer)
+  defp build_fields([], _level, _pretty?, _subfields?, buffer), do: Enum.reverse(buffer)
 
-  defp build_fields([{field, {arguments, sub_fields}} | rest], level, pretty?, buffer) do
+  defp build_fields(
+         [{field, {arguments, sub_fields}} | rest],
+         level,
+         pretty?,
+         true = subfields?,
+         buffer
+       ) do
     field = field |> to_string() |> indent(level, pretty?)
-    nested_fields = build_fields(sub_fields, level + 1, pretty?)
+    nested_fields = build_fields(sub_fields, level + 1, pretty?, subfields?)
     arguments = QueryArgument.build(arguments, pretty?)
     closing = indent(@closing[pretty?], level, pretty?)
 
     buffer = [closing, nested_fields, @opening[pretty?], arguments, field | buffer]
-    build_fields(rest, level + 1, pretty?, buffer)
+    build_fields(rest, level + 1, pretty?, subfields?, buffer)
   end
 
-  defp build_fields([{field, sub_fields} | rest], level, pretty?, buffer) do
+  defp build_fields([{field, sub_fields} | rest], level, pretty?, true = subfields?, buffer) do
     field = field |> to_string() |> indent(level, pretty?)
-    nested_fields = build_fields(sub_fields, level + 1, pretty?)
+    nested_fields = build_fields(sub_fields, level + 1, pretty?, subfields?)
     closing = indent(@closing[pretty?], level, pretty?)
 
     buffer = [closing, nested_fields, @opening[pretty?], field | buffer]
-    build_fields(rest, level + 1, pretty?, buffer)
+    build_fields(rest, level + 1, pretty?, subfields?, buffer)
   end
 
-  defp build_fields([field], level, pretty?, buffer) do
+  defp build_fields([{field, arguments} | rest], level, pretty?, false = subfields?, buffer) do
+    field = field |> to_string() |> indent(level, pretty?)
+    arguments = QueryArgument.build(arguments, pretty?)
+
+    buffer = [arguments, field | buffer]
+    build_fields(rest, level + 1, pretty?, subfields?, buffer)
+  end
+
+  defp build_fields([field], level, pretty?, _subfields?, buffer) do
     field = field |> to_string() |> indent(level, pretty?)
     field = if pretty?, do: [field, "\n"], else: field
 
     Enum.reverse([field | buffer])
   end
 
-  defp build_fields([field | rest], level, pretty?, buffer) do
+  defp build_fields([field | rest], level, pretty?, subfields?, buffer) do
     field = field |> to_string() |> indent(level, pretty?)
     separator = @separator[pretty?]
 
-    build_fields(rest, level, pretty?, [separator, field | buffer])
+    build_fields(rest, level, pretty?, subfields?, [separator, field | buffer])
   end
 
   defp indent(iodata, level, true), do: [String.duplicate("  ", level), iodata]
